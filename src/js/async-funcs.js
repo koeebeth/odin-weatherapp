d62d255098e58159474b9c7b6cdd0b93c60543d9
koeebeth <96339397+koeebeth@users.noreply.github.com>
2023-08-12 14:08:55 +0300

    Fixed cors requests

--- src/js/async-funcs.js
+++ src/js/async-funcs.js
@@ -1,5 +1,7 @@
 async function getCurWeather(location){
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=2dba2dd8ec534059a89172222230908&q=${location}`)
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=2dba2dd8ec534059a89172222230908&q=${location}`,{
+        mode: 'cors',
+    })
     .catch((error) => console.log(error));
     const weather = await response.json();
 
@@ -7,7 +9,9 @@ async function getCurWeather(location){
 }
 
 async function getForecast(location){
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=2dba2dd8ec534059a89172222230908&q=${location}&days=3`)
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=2dba2dd8ec534059a89172222230908&q=${location}&days=3`, {
+        mode: 'cors',
+    })
     .catch((error) => console.log(error));
     const forecast = await response.json();
     return forecast;
