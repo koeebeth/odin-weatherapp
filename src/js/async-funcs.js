13eee331af4c0b838f90202a12966a7d403fda6d
koeebeth <96339397+koeebeth@users.noreply.github.com>
2023-08-12 14:11:48 +0300

    FIxed http to https request

--- src/js/async-funcs.js
+++ src/js/async-funcs.js
@@ -1,5 +1,5 @@
 async function getCurWeather(location){
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=2dba2dd8ec534059a89172222230908&q=${location}`,{
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=2dba2dd8ec534059a89172222230908&q=${location}`,{
         mode: 'cors',
     })
     .catch((error) => console.log(error));
@@ -9,7 +9,7 @@ async function getCurWeather(location){
 }
 
 async function getForecast(location){
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=2dba2dd8ec534059a89172222230908&q=${location}&days=3`, {
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=2dba2dd8ec534059a89172222230908&q=${location}&days=3`, {
         mode: 'cors',
     })
     .catch((error) => console.log(error));
